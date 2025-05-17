8b3e661359321191a89139c0a3729409d8c04874
Yuliia <juliy9050@gmail.com>
2025-05-17 10:28:27 +0200

    add correction homework23

--- homework23/src/modules/Profile/Profile.jsx
+++ homework23/src/modules/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import PageTitle from "../../shared/components/PageTitle/PageTitle";
 import Button from "../../shared/components/Button/Button";
@@ -9,14 +10,16 @@ import { logoutUser } from "../../redux/auth/auth-thunks";
 const Profile = () => {
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         dispatch(logoutUser());
+        navigate("/login"); 
     }
 
     return (
         <div>
-            <PageTitle>Welcome to the Profile!</PageTitle>
+            <PageTitle>Welcome!</PageTitle>
             <Button onClick={handleLogout}>Logout</Button>
         </div>
     )
